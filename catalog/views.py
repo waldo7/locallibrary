db19d27295020b912cc98f0bdfd65ed57a369089
Waldo Ramones <waldo.ramones@gmail.com>
2019-01-21 22:15:04 +0800

    Session complete

--- catalog/views.py
+++ catalog/views.py
@@ -9,15 +9,14 @@ from catalog.models import Book, Author, BookInstance, Genre
 def index(request):
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-
     num_instances_available = BookInstance.objects.filter(
         status__exact='a').count()
-
     num_authors = Author.objects.count()
-
     num_genres = Genre.objects.filter(name__icontains='fiction').count()
-
     num_books_word_the = Book.objects.filter(title__icontains='wolf').count()
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
 
     context = {
         'num_books': num_books,
@@ -26,6 +25,7 @@ def index(request):
         'num_authors': num_authors,
         'num_genres': num_genres,
         'num_books_word_the': num_books_word_the,
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
