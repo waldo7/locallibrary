d76b0919030a2fd755a8024ab614c41fb080a7c6
waldo7 <waldo.ramones@gmail.com>
2019-01-21 14:45:53 +0800

    Finish book list and detail view

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from catalog.models import Book, Author, BookInstance, Genre
 
@@ -28,3 +29,12 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
