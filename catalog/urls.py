03700421209f62eff46454455f5599d8188a0075
Waldo Ramones <waldo.ramones@gmail.com>
2019-01-23 21:33:54 +0800

    Add renew form

--- catalog/urls.py
+++ catalog/urls.py
@@ -11,12 +11,17 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
-    path('author/', views.AuthorListView.as_view(), name='authors')
+    path('author/', views.AuthorListView.as_view(), name='authors'),
 ]
 
 urlpatterns += [
     path('mybooks/', views.LoadedBooksByUserListView.as_view(), name='my-borrowed'),
-    path('borrowed/', views.LoanedBookAllListView.as_view(), name='all-borrowed')
+    path('borrowed/', views.LoanedBookAllListView.as_view(), name='all-borrowed'),
+]
+
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian,
+         name='renew-book-librarian'),
 ]
 
 
